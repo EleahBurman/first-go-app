d725cfe91f63c917c118260d8eb19ebe64893a83
eleahburman <eleahjburman@gmail.com>
2024-04-18 15:29:50 -0400

    Add pointers for user to add information for confirmation

--- main.go
+++ main.go
@@ -7,17 +7,24 @@ func main(){
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	//fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your %v tickets here to attend\n", conferenceTickets)
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	//ask user for their name
 	fmt.Println("Enter your first name: ")
-	fmt.Scan(&userName)
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at: %v\n", firstName, lastName, userTickets, email)
 }
